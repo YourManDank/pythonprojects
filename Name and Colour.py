f7f72e5ffd2289ae4efdf274181334772624bcd8
YourManDank <yourmandank@gmail.com>
2024-11-12 23:18:42 +0000

    Initial Commit

---
+++ Name and Colour.py
@@ -0,0 +1,4 @@
+name = input('What is your name? ')
+print('Hi ' + name )#
+favourite_colour = input('What is your favourite colour?')
+print (name + ' likes ' + favourite_colour)
---
+++ String Methods.py
@@ -0,0 +1,5 @@
+course = 'Python for Beginners'
+print(len(course))
+
+# Printing a variable with the (len(variable)) prefix will print the numbers of characters in the variable.
+# We can tie this to a limiter that will, for example, prevent the input of an online form exceeding 20 characters.
